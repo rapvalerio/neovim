fc29c16ee372581459d065f7db10674f3f512c97
raphael valerio <raphael.valerio88@gmail.com>
2024-09-16 12:34:33 -0300

    nvim configs

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,32 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  require('gruvbox').setup {
+    terminal_colors = true, -- add neovim terminal colors
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = {
+      strings = true,
+      emphasis = true,
+      comments = true,
+      operators = false,
+      folds = true,
+    },
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    invert_intend_guides = false,
+    inverse = true, -- invert background for search, diffs, statuslines and errors
+    contrast = '', -- can be "hard", "soft" or empty string
+    palette_overrides = {},
+    overrides = {},
+    dim_inactive = false,
+    transparent_mode = false,
+  },
+
+  vim.cmd 'colorscheme gruvbox',
+}
